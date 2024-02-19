51070a582a2d92d80791374e3c4923b113265866
lahaina <nattolecats@gmail.com>
2024-02-19 17:43:35 +0900

    chatora: Fix Android.mk

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),X3-KC_sprout)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),chatora)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
