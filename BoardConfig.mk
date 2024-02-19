d34d1c1be42bfbce247de0d1cb48fdf77f3c6f30
lahaina <nattolecats@gmail.com>
2024-02-19 18:09:31 +0900

    chatora: Refactor kernel cmdlines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,21 @@ TARGET_SCREEN_DENSITY := 480
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=7 buildvariant=user veritykeyid=id:2faff71222990ad82af70d662fcfe6d556ab0df1
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 \
+    androidboot.console=ttyMSM0 \
+    earlycon=msm_serial_dm,0xc170000 \
+    androidboot.hardware=qcom \
+    user_debug=31 \
+    msm_rtb.filter=0x37 \
+    ehci-hcd.park=3 \
+    lpm_levels.sleep_disabled=1 \
+    sched_enable_hmp=1 \
+    sched_enable_power_aware=1 \
+    service_locator.enable=1 \
+    swiotlb=1 \
+    loop.max_part=7 \
+    buildvariant=user \
+    veritykeyid=id:2faff71222990ad82af70d662fcfe6d556ab0df1
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := X3-KC_sprout_defconfig
